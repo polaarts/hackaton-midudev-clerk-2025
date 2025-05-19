6594e1c652d98fe7db992194fb00a07c7c898494
Samuel Angulo <samuel.angulo_c@mail.udp.cl>
2025-05-19 18:45:04 -0400

    setting up react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from "astro/config";
 import node from '@astrojs/node'
 import clerk from '@clerk/astro'
 import tailwindcss from "@tailwindcss/vite";
+import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [clerk()],
+  integrations: [clerk(), react()],
   vite: {
     plugins: [tailwindcss()]
   },
